3c0bc2eaa56967de5cae4560ab2d62d2a2c107e2
Mohamed Reda <webzerone@gmail.com>
2025-09-04 20:20:30 +0300

    Adding Search

--- Queries.sql
+++ Queries.sql
@@ -1,8 +1,3 @@
 select * from tasks where user_id=2 and status='Backlog';
 select count(id) AS task_count,status from tasks group by status;
-select tasks.*,users.* from tasks JOIN users ON tasks.user_id=users.id order by users.id,tasks.id;;
-
-mytodo.infy.uk
-mytodo
-if0_39860621 
-3sxzn0e7rkdzhRm
+select tasks.*,users.* from tasks JOIN users ON tasks.user_id=users.id order by users.id,tasks.id;
